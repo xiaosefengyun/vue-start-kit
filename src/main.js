8e633a401b2f7bad89beb9be8874d942c26ba9b7
wangfei <wangfei@staff.easub.com>
2017-06-30 13:10:34 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 import router from './router';
 import store from './store/index';
 import i18n from './translations';
+
+Vue.use(ElementUI);
 
 
 Vue.config.productionTip = false;
--- src/pages/AppPage/index.vue
+++ src/pages/AppPage/index.vue
@@ -2,6 +2,7 @@
   <div>
     {{ $t('lang.hello') }}
     <Hello></Hello>
+    <el-button class="btn">Element按钮</el-button>
   </div>
 </template>
 <script>
@@ -17,6 +18,9 @@ export default {
 <style lang="scss">
   div {
     text-align: center;
+    margin-top: 40px;
+  }
+  .btn {
     margin-top: 40px;
   }
 </style>
