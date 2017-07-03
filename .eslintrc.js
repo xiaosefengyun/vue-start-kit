c3b4ffef1b07bdb809d83781a6887432997402ff
DESKTOP-5GIS19O\vince <xiaosefengyun@gmail.com>
2017-07-03 17:07:42 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,6 @@ module.exports = {
         }],
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
+        "linebreak-style": 'off'
     }
 }
